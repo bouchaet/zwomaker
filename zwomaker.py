a3abacac2033fb6edc9ac6ee289028bf5df56cc3
Etienne Bouhard <bouchaet@gmail.com>
2020-08-08 17:11:48 -0400

    refactoring. replace dict by classes

--- zwomaker.py
+++ zwomaker.py
@@ -1,142 +1,229 @@
 import re
 import xml.dom.minidom
+import argparse
 
-def dict_to_xml(d, text=None, is_open=False):
-    tmp = [item for item in d.items()]
-    elements = [i for i in tmp if i[0][0] != "@"]
-    attribs = [i for i in tmp if i[0][0] == "@"]
-    if not text:
-        text = []
 
-    for key, value in attribs:
-        text.append(f" {key[1:]}=\"{value}\"")
-    if is_open:
-        text.append(">")
-        is_open = False
+class ZwoElement():
+    def __init__(self, name, value=None):
+        self._name = name
+        self._attribs = []
+        self._elements = []
+        self._value = value
 
-    for key,value in elements:
-        text.append(f"<{key}")
-        if isinstance(value, dict):
-            dict_to_xml(value, text, True)
-        elif isinstance(value, list):
-            text.append(">")
-            for x in value:
-                dict_to_xml(x, text, False) 
-        else:
-            text.append(f">{value}")
-        text.append(f"</{key}>")
+    def to_xml(self):
+        att_str = "".join([f" {k}=\"{v}\"" for k, v in self._attribs])
+        subs = "".join([s.to_xml() for s in self._elements])
+        value = ""
+        if self._value:
+            value = self._value
+        return f"<{self._name}{att_str}>{value}{subs}</{self._name}>"
 
-    return "".join(str(t) for t in text)
+    def add_attrib(self, name, value):
+        self._attribs.append((name, value))
+
+    def add_element(self, element):
+        if element:
+            self._elements.append(element)
+
+    def get_duration(self):
+        return next((x for x in self._attribs if x[0].lower() == "duration"),
+                    None)
+
+    def set_duration(self, value):
+        for x in self._attribs:
+            if x[0].lower() == "duration":
+                x[1] = value
+
+    duration = property(get_duration, set_duration)
+
+
+class TextEvent(ZwoElement):
+    def __init__(self, distoffset, message):
+        super().__init__("textevent")
+        self.add_attrib("distoffset", distoffset)
+        self.add_attrib("message", message)
+
+
+class ZwoParser():
+    def __init__(self, pattern):
+        self._pattern = pattern
+
+    def can_parse(self, line):
+        regex = re.compile(self._pattern)
+        return regex.match(line)
+
+    def parse(self, line):
+        pass
+
+
+class Intervals(ZwoParser):
+    def __init__(self):
+        super().__init__(
+            "^I [0-9]* [0-9]*:[0-9]* [0-9]*:[0-9]*( [0-9](2,3))?")
+
+    def parse(self, line):
+        tokens = line.split()
+        repeat = int(tokens[1])
+        onDuration = int(tokens[2].split(':')[0])
+        offDuration = int(tokens[2].split(':')[1])
+
+        el = ZwoElement("IntervalsT")
+        el.add_attrib("Repeat", repeat)
+        el.add_attrib("OnDuration", onDuration)
+        el.add_attrib("OffDuration", offDuration)
+        el.add_attrib("OnPower", int(tokens[3].split(':')[0])/100)
+        el.add_attrib("OffPower", int(tokens[3].split(':')[1])/100)
+        el.add_attrib("Cadence", int(int(tokens[4])/2))
+
+        for x in range(repeat):
+            offset = onDuration + x * (onDuration + offDuration) - 100
+            if offset > 100:
+                el.add_element(
+                    TextEvent(offset, "Last 100 meters. Keep it up!"))
+        return el
+
+
+class SteadyState(ZwoParser):
+    def __init__(self):
+        super().__init__(
+            "^S [0-9]* [0-9]*"
+        )
+
+    def parse(self, line):
+        tokens = line.split()
+        el = ZwoElement("SteadyState")
+        el.add_attrib("Duration", tokens[1])
+        el.add_attrib("Power", int(tokens[2].split(':')[0])/100)
+        el.add_attrib("Cadence", int(int(tokens[3])/2))
+        return el
+
+
+class Ramp(ZwoParser):
+    def __init__(self):
+        super().__init__(
+            "^R [0-9]* [0-9]*:[0-9]*( [0-9](2,3))?"
+        )
+
+    def parse(self, line):
+        tokens = line.split()
+        el = ZwoElement("Ramp")
+        el.add_attrib("Duration", tokens[1])
+        el.add_attrib("PowerLow", int(tokens[2].split(':')[0])/100)
+        el.add_attrib("PowerHigh", int(tokens[2].split(':')[1])/100)
+        el.add_attrib("Cadence", 90)
+        return el
+
+
+class Warmup(ZwoParser):
+    def __init__(self):
+        super().__init__("^W [0-9]* [0-9]*:[0-9]*")
+
+    def parse(self, line):
+        tokens = line.split()
+        el = ZwoElement("Warmup")
+        el.add_attrib("Duration", tokens[1])
+        el.add_attrib("PowerLow", int(tokens[2].split(':')[0])/100)
+        el.add_attrib("PowerHigh", int(tokens[2].split(':')[1])/100)
+        return el
+
+
+class Cooldown(ZwoParser):
+    def __init__(self):
+        super().__init__("^C [0-9]*")
+
+    def parse(self, line):
+        tokens = line.split()
+        el = ZwoElement("Cooldown")
+        el.add_attrib("Duration", tokens[1])
+        el.add_attrib("PowerLow", int(tokens[2].split(':')[0])/100)
+        el.add_attrib("PowerHigh", int(tokens[2].split(':')[1])/100)
+        return el
+
+
+class Name(ZwoParser):
+    def __init__(self):
+        super().__init__("^N .*")
+
+    def parse(self, line):
+        tokens = line.split()
+        return ZwoElement("name", tokens[1])
 
 
 def lex(zwo_spec):
-    token_exprs = [
-            ("^I [0-9]*:[0-9]* [0-9]*:[0-9]*( [0-9](2,3))?", "IntervalsT", addInterval),
-            ("^R .*", "Ramp", addRamp),
-            ("^S [0-9]* [0-9]*", "SteadyState", addSteadyState),
-            ("^W [0-9]* [0-9]*:[0-9]*", "Warmup", addWarmup),
-            ("^C [0-9]*", "Cooldown", addCooldown),
-            ("^T .*", "Name", addName),
-            ("^# .*", "Comment", comment)
-            ]
-    match = None
-    wo = {"zwift_workout": {"name":"", "sportType":"run", "workout":[]}}
+    wof = ZwoElement("workout_file")
+    wo = ZwoElement("workout")
+    parser_actions = [
+        (Intervals(), wo.add_element),
+        (Ramp(), wo.add_element),
+        (Name(), wof.add_element),
+        (Warmup(), wo.add_element),
+        (Cooldown(), wo.add_element),
+        (SteadyState(), wo.add_element)
+    ]
+
     for line in zwo_spec.splitlines():
-        print(f"processing {line}...")
-        for token_expr in token_exprs:
-            pattern, tag, action = token_expr
-            regex = re.compile(pattern)
-            match = regex.match(line)
+        if not len(line):
+            continue
 
-            if match:
-                print(f"{match.group(0)} match with tag {tag}")
-                action(wo, line)
+        print(f"processing line {line}...")
+        match = False
+        for parser, action in parser_actions:
+            if parser.can_parse(line):
+                action(parser.parse(line))
+                print(f"Match with {parser.__class__.__name__}")
+                match = True
                 continue
-    return wo
+        if not match:
+            print("*** NO MATCH ***")
+
+    wof.add_element(ZwoElement("sportType", "run"))
+    wof.add_element(get_tags())
+    wof.add_element(wo)
+    return wof
 
 
-def comment(wo, line):
-    pass
-
-def addSteadyState(wo, line):
-    tokens = line.split()
-    el = {
-            "SteadyState": {
-                "@Duration": tokens[1],
-                "@Power": int(tokens[2].split(':')[0])/100,
-                "@Cadence": int(int(tokens[3])/2)
-                }}
-    wo["zwift_workout"]["workout"].append(el)
-
-def addWarmup(wo, line):
-    tokens = line.split()
-    el = {
-            "Warmup": {
-                "@Duration": tokens[1],
-                "@PowerLow": int(tokens[2].split(':')[0])/100,
-                "@PowerHigh": int(tokens[2].split(':')[1])/100,
-            }}
-    wo["zwift_workout"]["workout"].append(el)
-
-
-def addCooldown(wo, line):
-    tokens = line.split()
-    el = {
-            "Cooldown": {
-                "@Duration": tokens[1],
-                "@PowerLow": int(tokens[2].split(':')[0])/100,
-                "@PowerHigh": int(tokens[2].split(':')[1])/100,
-            }}
-    wo["zwift_workout"]["workout"].append(el)
-
-def addName(wo, line):
-    tokens = line.split();
-    wo["zwift_workout"]["name"] = tokens[1]
-
-def addInterval(wo, line):
-    tokens = line.split()
-    onDuration = tokens[1].split(':')[0]
-    offDuration = tokens[1].split(':')[1]
-    el = {
-        "IntervalsT": {
-            "@onDuration": onDuration,
-            "@offDuration": offDuration,
-            "@onPower": int(tokens[2].split(':')[0])/100,
-            "@offPower": int(tokens[2].split(':')[1])/100,
-            "@Cadence": int(int(tokens[3])/2)
-        }
-    }
-    wo["zwift_workout"]["workout"].append(el)
-
-def addRamp(wo, line):
-    tokens = line.split()
-    el = {
-            "Ramp": {
-                "@Duration": tokens[1],
-                "@PowerLow": int(tokens[2].split(':')[0])/100,
-                "@PowerHigh": int(tokens[2].split(':')[1])/100,
-                "@Cadence": 90
-                }}
-    wo["zwift_workout"]["workout"].append(el)
+def get_tags():
+    tags = ZwoElement("tags")
+    tags.add_element(ZwoElement("tag", "ZwoMaker"))
+    tags.add_element(ZwoElement("tag", "Running"))
+    return tags
+    
 
 def pretty_print(xml_string):
     dom = xml.dom.minidom.parseString(xml_string)
     return dom.toprettyxml()
 
-def create_zwo(zwo):
-    xml_text = dict_to_xml(zwo)
-    print(xml_text)
-    print(pretty_print(xml_text))
 
-SAMPLE="""
-T Test
+SAMPLE = """
+N Sample Workout
 W 1000 65:75
 S 1000 82 180
-I 1000:200 90:65 180
+I 2 1000:200 90:65 180
 R 1000 65:82 180
 C 1000 75:65
 """
 
-if __name__ == "__main__" :
-    wo = lex(SAMPLE)
-    create_zwo(wo)
+
+def main():
+    parser = argparse.ArgumentParser(description="Zwift worker maker.")
+    parser.add_argument("-s", "--specfile", type=str)
+    parser.add_argument("-o", "--output", type=str, default="out.zwo")
+    parser.add_argument("-p", "--print", action="store_true")
+
+    args = parser.parse_args()
+    spec = SAMPLE
+    if args.specfile:
+        with open(args.specfile, "r") as file:
+            spec = file.read()
+    
+    wof = lex(spec)
+    pp_xml = pretty_print(wof.to_xml())
+    with open(args.output, "w") as outfile:
+        outfile.write(pp_xml)
+    
+    if args.print:
+        print(pp_xml)
+
+
+if __name__ == "__main__":
+    main()
